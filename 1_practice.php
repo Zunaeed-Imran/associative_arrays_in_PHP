7a35907129476e6055a161a2743d568ee3b26b66
imran <zunaeede@gmail.com>
2023-12-06 10:37:18 +0600

    practice - i practice by isset

--- 1_practice.php
+++ 1_practice.php
@@ -1,4 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Practice associative array</title>
+</head>
+<body>
+  <form action="1_practice.php" method="post">
+    <input type="text" name="hero">
+    <button type="submit" name="submit">Submit</button>
+  </form>
+</body>
+</html>
+
 <?php
 
+  $marvel = array("doctor" => "Doctor Strange Sorcerer supreme",
+                   "stark" => "Tony Stark The genious Mechanic"               
+                );
 
+                $marvel = $marvel[$_POST['hero']];
+
+                if(isset($_POST['submit'])){
+                  echo $marvel;
+                }
 ?>
